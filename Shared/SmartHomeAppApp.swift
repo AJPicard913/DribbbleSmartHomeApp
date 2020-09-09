4460a7a16d760da7e4a538bd54b5992c04495687
ajpicard913 <Contact@saucedesigns.io>
2020-09-09 12:56:28 -0400

    Initial Commit

---
+++ Shared/SmartHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeAppApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 9/9/20.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
